b168f34f516243164de56cde9c0f6e8bc6bdb125
Manucher <manucher20056@gmail.com>
2023-10-11 14:20:31 +0500

    '.'

--- src/components/FormOTP.tsx
+++ src/components/FormOTP.tsx
@@ -1,5 +1,8 @@
+import { useStore } from '@/storage'
+import clsx from 'clsx'
 import { Input, Paragraph } from 'kuui-react'
 import { FC, HTMLAttributes, useState } from 'react'
+import { AlertError } from '.'
 
 export type TFormOTP = HTMLAttributes<HTMLDivElement>
 
@@ -9,18 +12,39 @@ export interface IFormOTP extends TFormOTP {
 
 export const FormOTP: FC<IFormOTP> = ({ className, email, ...props }) => {
 	const [otp, setOtp] = useState<string>('')
+	const [serverError, setServerError] = useState<string>('')
+	const setLoading = useStore(store => store.setLoading)
 
 	function otpChangeHandler(val: string) {
 		setOtp(val)
 	}
 
+	async function completeHandler() {
+		setLoading(true)
+		console.log(otp)
+		setLoading(false)
+	}
+
+	function serverErrorTimeHandler() {
+		setServerError('')
+	}
+
+	const styles = clsx([
+		'max-w-md w-full px-2 flex flex-col gap-4 items-center',
+		className,
+	])
+
 	return (
-		<div
-			className="max-w-md w-full px-2 flex flex-col gap-4 items-center"
-			{...props}
-		>
+		<div className={styles} {...props}>
+			<AlertError error={serverError} onTimeUp={serverErrorTimeHandler} />
 			<Paragraph align="center">Enter the code sent to {email}.</Paragraph>
-			<Input variant="OTP" length={6} value={otp} onChange={otpChangeHandler} />
+			<Input
+				variant="OTP"
+				length={6}
+				value={otp}
+				onChange={otpChangeHandler}
+				onComplete={completeHandler}
+			/>
 		</div>
 	)
 }
--- src/pages/RestoreAccount.tsx
+++ src/pages/RestoreAccount.tsx
@@ -1,11 +1,11 @@
-import { FormEmail } from '@/components'
+import { FormEmail, FormOTP } from '@/components'
 import { FC } from 'react'
 
 export const RestoreAccount: FC = () => {
 	return (
 		<div className="w-full h-full flex justify-center items-center">
-			{/* <FormOTP email="test@gmail.com" /> */}
-			<FormEmail />
+			<FormOTP email="test@gmail.com" />
+			{/* <FormEmail /> */}
 		</div>
 	)
 }
